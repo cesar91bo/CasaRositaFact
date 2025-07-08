497458e6135dd5094d00f210f22dd1d9f37b3a4f
cesar91bo <cesar91bo@gmail.com>
2025-07-08 17:56:52 -0300

    Lógica para actualizar precio o cargar desde el formulario de facturación

---
+++ CasaRositaFact/Models/ArticuloModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CasaRositaFact.Models
+{
+    public class ArticuloModel
+    {
+        public int IdArticulo { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal? StockActual { get; set; }
+        public decimal PrecioActual { get; set; }
+    }
+}
